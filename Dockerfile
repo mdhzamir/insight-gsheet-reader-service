8e195f15f619eaa7e6451ff760815de1d965c06b
Moudud <moudud@batworld.com>
2020-03-04 11:30:16 +0600

    [ADD] eureka client

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM python:3.7.1
 
 WORKDIR /app
 COPY . /app
+RUN apt-get update
+RUN pip --no-cache-dir install -r requirements.txt                                                                      
+RUN pip install py_eureka_client
 EXPOSE 5001
 
 ENTRYPOINT  ["python3"]
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from src.controller.application_data import application_data
 from src.controller.main_data import main_data
+import py_eureka_client.eureka_client as eureka_client
 
 
 app = Flask(__name__)
@@ -10,7 +11,11 @@ app.register_blueprint(main_data)
 api = Api(app)
 
 
-
+your_rest_server_port = 5001
+eureka_client.init(eureka_server="http://insight-discovery-service:8761/eureka/",
+                                app_name="python-client",
+                                instance_port=your_rest_server_port,
+                                instance_id ="python-client:5001")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000, debug=True)
