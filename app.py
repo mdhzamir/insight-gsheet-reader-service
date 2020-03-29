546446612b4acb3321e89f43dea9c5dc677bb40b
shakib ahmed <shakib@batworld.com>
2020-03-29 14:12:10 +0600

    ADD eureka

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from src.controller.application_data import application_data
 from src.controller.main_data import main_data
 from src.controller.covid_data import covid_data
-# import py_eureka_client.eureka_client as eureka_client
+import py_eureka_client.eureka_client as eureka_client
 
 
 app = Flask(__name__)
@@ -14,11 +14,11 @@ app.config['JSON_SORT_KEYS'] = False
 api = Api(app)
 
 
-# your_rest_server_port = 5001
-# eureka_client.init(eureka_server="http://insight-discovery-service:8761/eureka/",
-#                                 app_name="insight-gsheet-reader-service",
-#                                 instance_port=your_rest_server_port,
-#                                 instance_id ="insight-gsheet-reader-service:5001")
+your_rest_server_port = 5001
+eureka_client.init(eureka_server="http://insight-discovery-service:8761/eureka/",
+                                app_name="insight-gsheet-reader-service",
+                                instance_port=your_rest_server_port,
+                                instance_id ="insight-gsheet-reader-service:5001")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5001, debug=True)
